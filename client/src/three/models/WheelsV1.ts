453d3be9763a7ff7a2b6f966c3e0aa5bb4d1119c
Dasha <daryna2003tk@gmail.com>
2026-03-30 13:50:50 +0200

    fix

--- client/src/three/models/WheelsV1.ts
+++ client/src/three/models/WheelsV1.ts
@@ -15,38 +15,34 @@ export class WheelsV1Model {
     onLoad?: ModelLoadCallback
   ): Promise<WheelsArray> {
     const wheelModel = await this.loader.loadModel('/models/Wheels/v1/tuner_wheel.glb');
-    
-    // Setup wheel model
+
     const scale = this.getScale();
     wheelModel.scale.set(scale, scale, scale);
     wheelModel.rotateY(Math.PI * -0.5);
-    
+
     this.loader.setupShadows(wheelModel, false, true);
-    
-    // Create wheel instances
+
     const positions = this.getPositions(isPickup);
     const wheels: WheelsArray = [];
-    
+
     positions.forEach((pos, index) => {
       const wheelClone = wheelModel.clone();
       wheelClone.position.copy(pos);
-      
-      // Rotate rear wheels
+
       if (index === 2 || index === 3) {
         wheelClone.rotateY(Math.PI);
       }
-      
+
       scene.add(wheelClone);
       wheels.push(wheelClone);
     });
-    
-    // Add update method
+
     wheels.updatePositions = (isPickup: boolean) => {
       this.updatePositions(wheels, isPickup);
     };
-    
+
     if (onLoad) onLoad(wheels as unknown as THREE.Object3D);
-    
+
     return wheels;
   }
 
